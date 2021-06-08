c1b8fe8130c5c73ac90bbe4a48b80b049fc7935a
Mateo Ferreira <matteo948@icloud.com>
2021-06-08 15:35:48 -0400

    image card destrcutured

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from "react";
 
- const ImageCard = () => {
-    return (
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
 
-<div className="max-w-sm rounded overflow-hidden shadow-lg ">
-<img src="https://source.unsplash.com/random" 
-alt="" 
-className="w-full"/>
-<div className="px-6 py-4">
-  <div className="font-bold text-purple-500 text-xl mb-2">
-    photo by Jhon doe 
-  </div>
-  <ul>
-    <li>
-    <strong>views: 4000</strong>
-    </li>
-    <li>
-    <strong>downloads: 300</strong>
-    </li>
-    <li>
-    <strong>likes: 200</strong>
-    </li>
-  </ul>
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+      <img src={image.webformatURL} alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          photo by {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>views: {image.views}</strong>
+          </li>
+          <li>
+            <strong>downloads: {image.downloads}</strong>
+          </li>
+          <li>
+            <strong>likes: 200</strong>
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+       
+    {tags.map( tag=> ( <span key={image.id} className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #{tag}
+        </span>) )}
+      </div>
+    </div>
+  );
+};
 
-</div>
-<div className="px-6 py-4">
-  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
-</div>
-</div>
-    )
-}
-
-export default ImageCard
+export default ImageCard;
