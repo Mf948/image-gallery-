7e7797d4821c1b10c32d44b9c244677cadfcfebc
Mateo Ferreira <matteo948@icloud.com>
2021-06-08 13:17:25 -0400

    component layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div className="max-w-sm rounded overflow-hidden shadow-lg ">
+<img src="https://source.unsplash.com/random" 
+alt="" 
+className="w-full"/>
+<div className="px-6 py-4">
+  <div className="font-bold text-purple-500 text-xl mb-2">
+    photo by Jhon doe 
+  </div>
+  <ul>
+    <li>
+    <strong>views: 4000</strong>
+    </li>
+    <li>
+    <strong>downloads: 300</strong>
+    </li>
+    <li>
+    <strong>likes: 200</strong>
+    </li>
+  </ul>
+
+</div>
+<div className="px-6 py-4">
+  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+  <span className="inline-block bg-gray-200 rounded-full px3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+</div>
+</div>
   );
 }
 
